493b7eac31596012a1e95581cef0e0f5290a73bd
Md Shaheen Anwar <shaheen.anwar@tavant.com>
2021-06-01 11:59:45 +0530

    processIntance time

--- src/main/java/com/demo/workflow/LoggerDelegate.java
+++ src/main/java/com/demo/workflow/LoggerDelegate.java
@@ -2,7 +2,10 @@ package com.demo.workflow;
 
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.camunda.bpm.engine.history.HistoricProcessInstance;
+import org.camunda.bpm.engine.runtime.ProcessInstance;
 
+import java.time.LocalDateTime;
 import java.util.Date;
 import java.util.logging.Logger;
 
@@ -12,11 +15,18 @@ public class LoggerDelegate implements JavaDelegate {
     @Override
     public void execute(DelegateExecution delegateExecution) throws Exception {
 
+        LocalDateTime dateTime = LocalDateTime.now();
+        delegateExecution.setVariable("Date: ",dateTime);
         LOGGER.info("Invoked By Id: " +delegateExecution.getProcessInstanceId()
             +", Activity Name: "+ delegateExecution.getCurrentActivityName()
                 +"\n Date: "+new Date()+"\n Loan Requester: "
                 +delegateExecution.getVariable("Loan Requester"));
-
+        LOGGER.info("Date: "+delegateExecution.getVariable("dateTime"));
+        //Process Start time
+        HistoricProcessInstance pi = delegateExecution.getProcessEngine()
+                .getHistoryService()
+                .createHistoricProcessInstanceQuery()
+                .processInstanceId(delegateExecution.getProcessInstanceId()).singleResult();
 
     }
 }
